800ee48b524479f07fb0076747803691d7d5977c
AgnirudraSil <69184486+AgnirudraSil@users.noreply.github.com>
2020-11-01 23:58:49 +0530

    Final Commit

--- routes/auth.js
+++ routes/auth.js
@@ -7,17 +7,6 @@ const authController = require('../controllers/auth')
 const router = express.Router()
 
 router.put('/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Please enter a valid email.')
-        .custom((value, { req }) => {
-            return User.findOne({ email: value }).then(userDoc => {
-                if (userDoc) {
-                    return Promise.reject('E-Mail address already exists!')
-                }
-            })
-        })
-        .normalizeEmail(),
     body('password').trim().isLength({ min: 5 }),
     body('name')
         .trim()
@@ -26,6 +15,6 @@ router.put('/signup', [
 ], authController.signup
 )
 
-router.post('/login',  authController.login)
+router.post('/login', authController.login)
 
 module.exports = router
